0bc64152ce265bdd2eaab907a7603f8a4a32da88
ffraniel <francis.whitehead@googlemail.com>
2018-05-16 22:51:47 +0100

    changed firebase script reference to more accurately request just the DB package

--- public/js/fade-transition.js
+++
@@ -1,1 +0,0 @@
-var links=document.querySelectorAll(".folio-item a");links.forEach(e=>{e.addEventListener("click",o=>{o.preventDefault(),document.body.classList.add("fade-out-again"),setTimeout(()=>{document.location=e.href},250)})}),window.onload=(()=>{document.body.classList.remove("fade-out")});
